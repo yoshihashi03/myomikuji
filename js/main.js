fb12197251a3954e95e86843460caeb630d7aa7d
吉橋かすみ <yoshihashikasumi@MacBook-Pro.local>
2020-11-14 12:01:13 +0900

    finish

--- js/main.js
+++ js/main.js
@@ -4,19 +4,17 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    const n = Math.floor(Math.random() * 3)
-    // btn.textContent = n;
+    // const results = ['大吉', '中吉', '凶', '末吉']
+    // const results = ['大吉', '大吉', '凶', '大吉']
+    // btn.textContent = results[ Math.floor(Math.random() * results.length)];
 
-    switch(n) {
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '中吉';
-        break;
-      case 2:
-        btn.textContent = '凶';
-        break;
-    }
-  })
+    const n = Math.random();
+    if (n < 0.05) {
+      btn.textContent = '大吉'; // 5%
+    }else if (n < 0.2) {
+        btn.textContent = '中吉'; // 15%
+    }else {
+        btn.textContent = '凶'; // 80%
+      }
+})
 }
